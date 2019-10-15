f6956df687165271ab7d82d5ba41c42ddc28918d
Sarah Tatro <statrox4@gmail.com>
2019-10-15 12:49:07 -0600

    Update search endpoints to include secured api info & proper keys in fetch calls

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -1,49 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require("node-fetch");
-// var db = require("database");
 var express = require('express');
-// var getAvgCalories = require('getAvgCalories');
 var router = express.Router();
 var Recipe = require('../../../models').Recipe
 var Sequelize = require('sequelize')
 require('dotenv').config();
 
 router.get('/calorie_search', function(req, res, next) {
-    then (recipes => fetch(`https://api.edamam.com/search?q=chicken&app_id=API_ID&app_key=API_KEY&from=0&to=1&calories=591-722&health=alcohol-free`))
-    .then(response => response.json())
-    .then(response => {
-    res.status(200).send(payload)
+    fetch(`https://api.edamam.com/search?q=${req.query.label}&app_id=${process.env.app_id}&app_key=${process.env.app_key}&from=0&to=1&calories=${req.query.calories}`)
+    .then(response =>
+      response.json())
+    .then(results => {
+    res.status(200).send(results)
     })
   })
 
 router.get('/health_label_search', function(req, res, next) {
-    then (recipes => fetch(`https://api.edamam.com/search?q=beef&app_id=API_ID&app_key=API_KEY&from=0&to=1&dietLabels=gluten-free&health=alcohol-free`))
-    .then(response => response.json())
-    .then(response => {
-    res.status(200).send(payload)
+    fetch(`https://api.edamam.com/search?q=${req.query.label}&app_id=${process.env.app_id}&app_key=${process.env.app_key}&from=0&to=1&diet=${req.query.dietLabel}`)
+    .then(response =>
+      response.json())
+    .then(results => {
+    res.status(200).send(results)
     })
   })
 
 router.get('/servings_search', function(req, res, next) {
-    then (recipes => fetch(`https://api.edamam.com/search?q=beef&app_id=API_ID&app_key=API_KEY&from=0&to=1&yield=6&health=alcohol-free`))
-    .then(response => response.json())
-    .then(response => {
-    res.status(200).send(payload)
+    fetch(`https://api.edamam.com/search?q=${req.query.label}&app_id=${process.env.app_id}&app_key=${process.env.app_key}&from=0&to=1&yield=${req.query.yield}`)
+    .then(response =>
+      response.json())
+    .then(results => {
+    res.status(200).send(results)
     })
   })
 
 router.get('/average_calories', async function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
-
-  let response = await Recipe.findAll({
-    where: {
-      label: "chicken",
-      // req.params.label
-      attributes: [[Sequelize.fn('AVG', Sequelize.col('calories')), 'avgCalories']],
-    }
-    payload = {}
-  })
+    let response = await Recipe.findAll({
+      attributes: [[Sequelize.fn('AVG', Sequelize.col('calories')), 'avgCalories']]
+    })
   res.status(200).send(response)
 })
 
@@ -51,15 +46,10 @@ router.get('/order_servings', async function(req, res, next) {
     res.set("Connection", "close");
     let response = await Recipe.findAll({
       order: [
-        ['yield', 'DESC'],
-        ['id', 'ASC']
+        ['yield', 'DESC']
       ]
     })
-    payload = {}
-    response.forEach(function(element) {
-      payload["yield"] = element.yield
-    })
-    res.status(200).send(payload)
+    res.status(200).send(response)
   })
 
 module.exports = router;
